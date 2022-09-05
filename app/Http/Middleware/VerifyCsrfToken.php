b5459c657ad6285f1777e7c7c5d008703ffc9a0a
ag171980 <ag171980@gmail.com>
2022-09-05 00:06:58 -0300

    edit verifyCsrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        '/usuarios',
         '/usuarios/createAccount',
         '/usuarios/login',
         '/playlists/addSongToPlaylist',
